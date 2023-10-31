d95aba844a0af007baefe18af5142c4495be13d7
ubertron <andy_j_davis@yahoo.com>
2023-10-31 23:22:27 +0000

    Simplified robotools_shelf.py

--- plug-ins/robotools_shelf.py
+++ plug-ins/robotools_shelf.py
@@ -1,4 +1,3 @@
-# https://help.autodesk.com/view/MAYAUL/2023/ENU/?guid=Maya_SDK_A_First_Plugin_Python_HelloWorldAPI2_html
 import os
 import sys
 import maya.api.OpenMaya as om
@@ -9,7 +8,7 @@ import platform
 from maya import mel
 from pathlib import Path
 
-import robotools.robotools_utils
+from robotools import robotools_utils
 
 
 def maya_useNewAPI():
@@ -42,8 +41,10 @@ def initializePlugin(plugin):
     vendor = 'Robonobo'
     version = '1.0.0'
     pluginFn = om.MFnPlugin(plugin, vendor, version)
+
     try:
-        bootstrap()
+        robotools_utils.setup_robotools_shelf()
+        logging.info('-------- Initializing Robotools Shelf --------')
         pluginFn.registerCommand(RobotoolsShelfInitializeCmd.kPluginCmdName, RobotoolsShelfInitializeCmd.cmdCreator)
     except RuntimeError:
         raise RuntimeError('Failed to register command: %s\n' % RobotoolsShelfInitializeCmd.kPluginCmdName)
@@ -55,28 +56,10 @@ def uninitializePlugin(plugin):
     @param plugin:
     """
     pluginFn = om.MFnPlugin(plugin)
+
     try:
-        teardown()
+        robotools_utils.delete_robotools_shelf()
+        logging.info('-------- Deleting Robotools Shelf --------')
         pluginFn.deregisterCommand(RobotoolsShelfInitializeCmd.kPluginCmdName)
     except RuntimeError:
         raise RuntimeError('Failed to unregister command: %s\n' % RobotoolsShelfInitializeCmd.kPluginCmdName)
-
-
-def bootstrap():
-    """
-    Initialize Robotools Shelf
-    """
-    from robotools.utils import shelf_manager
-
-    robotools.robotools_utils.setup_robotools_shelf()
-    logging.info('-------- Initializing Robotools Shelf --------')
-
-
-def teardown():
-    """
-    Delete Robotools Shelf
-    """
-    from robotools.utils import shelf_manager
-
-    robotools.robotools_utils.delete_robotools_shelf()
-    logging.info('-------- Deleting Robotools Shelf --------')
